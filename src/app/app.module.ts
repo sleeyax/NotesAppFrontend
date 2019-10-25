8ebfc56c9590a9969d5a792c23791a95d2298ba3
stijnhhh <r0669730@student.thomasmore.be>
2019-10-25 11:23:25 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardModule} from './dashboard/dashboard.module';
+import { DashboardComponent} from './dashboard/dashboard/dashboard.component';
+import { LoginModule} from './login/login.module';
+import { LoginComponent} from './login/login/login.component';
+import { RegisterComponent} from './login/register/register.component';
+
+const appRoutes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   declarations: [
@@ -10,7 +21,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DashboardModule,
+    LoginModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
