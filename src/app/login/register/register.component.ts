cc4e0852df75e513aeabe6731bba3d6bb5021814
Sleeyax <yourd3veloper@gmail.com>
2019-11-17 16:35:54 +0100

    Refactor code a bit

--- src/app/login/register/register.component.ts
+++ src/app/login/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import {User} from '../../models/user.model';
+import {ApiService} from "../../services/api.service";
 
 @Component({
   selector: 'app-register',
@@ -11,9 +12,7 @@ export class RegisterComponent implements OnInit {
 
   registrationForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private _api: ApiService) {
     this.registrationForm = new FormGroup({
       username: new FormControl('', {validators: [Validators.required, Validators.minLength(4)]}),
       email: new FormControl('', {validators: [Validators.required, Validators.email]}),
@@ -22,11 +21,17 @@ export class RegisterComponent implements OnInit {
     });
   }
 
-  onRegistrate()
-  {
-    user: User;
-    if (this.registrationForm)
-    console.log("joow");
-  }
+  ngOnInit() {}
 
+  onRegisterFormSubmit()
+  {
+    // TODO: create user object and submit it using the api service
+    //       if success, redirect to login page OR send another request to log the user in and redirect to dashboard instead
+    //       if failed, show error message to the user
+    const form = this.registrationForm.value;
+    console.log(form);
+    // const user = new User(form.firstName, form.lastName, form.email, form.password);
+    // console.log(user);
+    // ...
+  }
 }
