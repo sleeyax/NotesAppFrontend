9bccab6b3afd3bfdde48dfeffed9c64360b0a40d
Sleeyax <yourd3veloper@gmail.com>
2019-12-02 23:10:22 +0100

    Move note service to api service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,13 +4,13 @@ import {HttpClient} from "@angular/common/http";
 import {TokenResponse} from "../models/token-response.model";
 import {Observable} from "rxjs";
 import {User} from "../models/user.model";
+import {Note} from "../models/note.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   private readonly _url: string = 'http://127.0.0.1:8080';
-  //private readonly _url: string = 'https://helpless-wombat-90.localtunnel.me';
   private readonly _services = {
     auth: 'auth',
     edge: 'listings'
@@ -32,5 +32,38 @@ export class ApiService {
    */
   createUser(user: User): Observable<User> {
     return this._httpClient.post<User>(`${this._url}/${this._services.auth}/users/add`, user);
+  }
+
+  /**
+   * Add a note
+   * @param note
+   */
+  createNote(note: Note): Observable<Note> {
+    return this._httpClient.post<Note>(`${this._url}/${this._services.edge}/notes/create`, note);
+  }
+
+  /**
+   * Fetch notes from specified user
+   * @param userid
+   */
+  getNotesByUserID(userid: number): Observable<Note[]> {
+    return this._httpClient.get<Note[]>(`${this._url}/${this._services.edge}/notes/search/userid=${userid}`);
+  }
+
+  /**
+   * Edit note
+   * @param note
+   */
+  updateNote(note : Note){
+    return this._httpClient.put<Note>(`${this._url}/${this._services.edge}/notes/update`, note);
+  }
+
+  /**
+   * Delete specified note from specified user
+   * @param noteId
+   * @param userId
+   */
+  deleteNote(noteId: string, userId: number) {
+    return this._httpClient.delete(`${this._url}/${this._services.edge}/notes/delete/${noteId}/user/${userId}`);
   }
 }
