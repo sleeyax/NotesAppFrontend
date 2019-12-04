a7138083f5271ba364be5d7e5c20d9557867825a
Dries Luyten <dries1luyten@hotmail.com>
2019-12-04 13:26:43 +0100

    account module

---
+++ src/app/account/account.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AccountManagementComponent } from './account-management/account-management.component';
+
+
+
+@NgModule({
+  declarations: [AccountManagementComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AccountModule { }
